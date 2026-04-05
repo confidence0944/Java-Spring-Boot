dff0ecd3c1d16570e61e15ca064f60ebc39dd087
leonHuang1123 <leon_huang@engsound.com.tw>
2026-04-05 21:36:34 +0800

    feat: init java spring boot

---
+++ src/main/java/training/web/Hello.java
@@ -0,0 +1,22 @@
+package training.web;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@RestController
+public class Hello {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Hello.class, args);
+    }
+
+    @GetMapping("/")
+    public String index()
+    {
+        return "Hello Spring boot web application.";
+    }
+}
